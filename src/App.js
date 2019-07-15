583eae45bb3f3fff0159c7da65961e3e6bf34cf5
Murray Walker <murray@colonyfilms.com>
2019-07-15 13:21:50 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./App.css";
+
+const App = x => {
+  const user = {
+    name: "Andrew",
+    cities: ["Phillie", "NYC"],
+    printPlaceLived() {
+      return this.cities.map(city => this.name + " has lived in " + city + " ");
+    }
+  };
+  return <div>{user.printPlaceLived()}</div>;
+};
+
 
 export default App;
